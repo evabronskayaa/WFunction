a347c471224a735bb0c3ce7e6d6d49f722fcaeb6
evabronskayaa <eva27122002b@gmail.com>
2021-10-21 01:56:54 +0500

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-WFunction
+## **Weierstrass Function**
+
+In mathematics, the Weierstrass function is an
+example of a real-valued function that is continuous 
+everywhere but differentiable nowhere. It is an example
+of a fractal curve. It is named after its discoverer 
+Karl Weierstrass.
+
+### _Visualisation_
+
+Visualization done with integration Python in C#
+
+```cs
+Environment.SetEnvironmentVariable("PYTHONNET_PYDLL",
+                                    @"C:\Users\Ева\AppData\Local\Programs\Python\Python39\python39.dll",
+                                    EnvironmentVariableTarget.Process);
+```
+
+Then the library "matplotlib" was imported
+
+```cs
+using (Py.GIL())
+{
+    dynamic mpl = Py.Import("matplotlib");
+    dynamic plt = Py.Import("matplotlib.pyplot");
+
+    plt.plot(XValues, YValues);
+    plt.show();
+}
+```
+
+Plot of Weierstrass function over the interval [−2, 2]
+
+![image1](https://raw.githubusercontent.com/evabronskayaa/WFunction/master/image1.png)
+
+### _Function_
+In Weierstrass's original paper, the function was defined as a Fourier series:
+
+![image2](https://wikimedia.org/api/rest_v1/media/math/render/svg/da584736d393e241fa5fa265cd17c06bc73f316e)
+
+```cs
+float res = 0;
+for (int n = 0; n < size; n++) 
+{
+    res += (float) (Math.Cos(Math.Pow(3, n) * Math.PI * x) / Math.Pow(2, n));
+}
+```
--- WFunction/Program.cs
+++ WFunction/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NumSharp;
 
 namespace WFunction
@@ -9,9 +10,9 @@ namespace WFunction
         static void Main(string[] args)
         {
             /// <summary>
-            /// Weierstrass function
-            /// Like other fractals, the function exhibits self-similarity:
-            /// every zoom  is similar to the global plot.
+            ///     Weierstrass function
+            ///     Like other fractals, the function exhibits self-similarity:
+            ///                         every zoom  is similar to the global plot.
             /// 
             /// </summary>
             
@@ -20,19 +21,19 @@ namespace WFunction
             Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", 
                                             @"C:\Users\Ева\AppData\Local\Programs\Python\Python39\python39.dll", 
                                                 EnvironmentVariableTarget.Process);
-            int size = 1000;
-            int startX = -1;
-            int stopX = 1;
+            int size = 10000;
+            int startX = -2;
+            int stopX = 2;
 
-            List<float> xValues = new List<float>();
-
-            for (float i = startX; i < stopX; i += (float)(stopX - startX) / size) {
+            var xValues = new List<float>();
+            for (float i = startX; i < stopX; i += (float)(stopX - startX) / size) 
+            {
                 xValues.Add(i);
             } 
             
             /// <summary>
             /// <param name="xValues">
-            ///     Also can loof like:
+            ///     Also can look like:
             ///             var xValues = np.arange(startX,stopX,(float)(stopX-startX)/size);
             ///
             ///     It's the analogue NumPy in C# (NumSharp)
@@ -41,19 +42,13 @@ namespace WFunction
             /// </summary>
             
             float[] x = xValues.ToArray();
-            
-            var y = new List<float>();
-            foreach (var floatX in x) 
-            {
-                y.Add(GetValue(500, floatX));
-            }
-            CreatePlot(x, y.ToArray());
+            CreatePlot(x, x.Select(floatX => GetValue(500, floatX)).ToArray());
         }
 
         private static float GetValue(int size, float x)
         {
             float res = 0;
-            for (int n = 0; n < size; n++)
+            for (int n = 0; n < size; n++) 
             {
                 res += (float) (Math.Cos(Math.Pow(3, n) * Math.PI * x) / Math.Pow(2, n));
             }
